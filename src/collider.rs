357cdb4787e22527927128256c3c08f1bffee056
VoxanyNet <kruzerbroek@gmail.com>
2024-05-06 17:06:26 -0400

    05-06-2024

--- src/collider.rs
+++ src/collider.rs
@@ -1,3 +1,118 @@
 use crate::proxies::macroquad::math::rect::Rect;
 
 pub type Collider = Rect;
+
+impl From<rapier2d::dynamics::RigidBody> for RigidBody {
+    fn from(value: rapier2d::dynamics::RigidBody) -> Self {
+        Self {
+            position: Vec2::new(value.position().translation.x, value.position().translation.y),
+            velocity: Vec2::new(value.linvel().x, value.linvel().y),
+            body_type: value.body_type().into()
+        }
+    }
+}
+
+impl From<&mut rapier2d::dynamics::RigidBody> for RigidBody {
+    fn from(value: &mut rapier2d::dynamics::RigidBody) -> Self {
+        Self {
+            position: Vec2::new(value.position().translation.x, value.position().translation.y),
+            velocity: Vec2::new(value.linvel().x, value.linvel().y),
+            body_type: value.body_type().into()
+        }
+    }
+}
+
+
+impl Into<rapier2d::dynamics::RigidBody> for RigidBody {
+    fn into(self) -> rapier2d::dynamics::RigidBody {
+
+        match self.body_type {
+            RigidBodyType::Dynamic => {
+                RigidBodyBuilder::dynamic()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::Fixed => {
+                RigidBodyBuilder::fixed()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicPositionBased => {
+                RigidBodyBuilder::kinematic_position_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicVelocityBased => {
+                RigidBodyBuilder::kinematic_velocity_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+        }
+    }
+}
+
+impl Into<rapier2d::dynamics::RigidBody> for &RigidBody {
+    fn into(self) -> rapier2d::dynamics::RigidBody {
+        match self.body_type {
+            RigidBodyType::Dynamic => {
+                RigidBodyBuilder::dynamic()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::Fixed => {
+                RigidBodyBuilder::fixed()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicPositionBased => {
+                RigidBodyBuilder::kinematic_position_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicVelocityBased => {
+                RigidBodyBuilder::kinematic_velocity_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+        }
+    }
+}
+
+impl Into<rapier2d::dynamics::RigidBody> for &mut RigidBody {
+    fn into(self) -> rapier2d::dynamics::RigidBody {
+        match self.body_type {
+            RigidBodyType::Dynamic => {
+                RigidBodyBuilder::dynamic()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::Fixed => {
+                RigidBodyBuilder::fixed()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicPositionBased => {
+                RigidBodyBuilder::kinematic_position_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicVelocityBased => {
+                RigidBodyBuilder::kinematic_velocity_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+        }
+    }
+}
--- src/rigid_body.rs
+++ src/rigid_body.rs
@@ -1,9 +1,8 @@
 use diff::Diff;
-use macroquad::math::Vec2;
+use rapier2d::{dynamics::RigidBodyBuilder, na::vector};
 use serde::{Deserialize, Serialize};
 
-use crate::proxies::rapier2d::dynamics::RigidBody::RigidBody;
-
+use crate::proxies::macroquad::math::vec2::Vec2;
 
 #[derive(Serialize, Deserialize, Diff, PartialEq, Clone)]
 #[diff(attr(
@@ -11,8 +10,20 @@ use crate::proxies::rapier2d::dynamics::RigidBody::RigidBody;
 ))]
 pub enum RigidBodyType {
     Dynamic,
-    Static,
-    Kinematic
+    Fixed,
+    KinematicPositionBased,
+    KinematicVelocityBased
+}
+
+impl From<rapier2d::dynamics::RigidBodyType> for RigidBodyType {
+    fn from(value: rapier2d::dynamics::RigidBodyType) -> Self {
+        match value {
+            rapier2d::dynamics::RigidBodyType::Dynamic => RigidBodyType::Dynamic,
+            rapier2d::dynamics::RigidBodyType::Fixed => RigidBodyType::Fixed,
+            rapier2d::dynamics::RigidBodyType::KinematicPositionBased => RigidBodyType::KinematicPositionBased,
+            rapier2d::dynamics::RigidBodyType::KinematicVelocityBased => RigidBodyType::KinematicVelocityBased
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Diff, PartialEq, Clone)]
@@ -28,45 +39,114 @@ pub struct RigidBody {
 impl From<rapier2d::dynamics::RigidBody> for RigidBody {
     fn from(value: rapier2d::dynamics::RigidBody) -> Self {
         Self {
-            position: Vec2::new(value.position().translation.x, value.position().translation.y)
-            velocity: value.linvel()[0]
+            position: Vec2::new(value.position().translation.x, value.position().translation.y),
+            velocity: Vec2::new(value.linvel().x, value.linvel().y),
+            body_type: value.body_type().into()
         }
     }
 }
 
 impl From<&mut rapier2d::dynamics::RigidBody> for RigidBody {
     fn from(value: &mut rapier2d::dynamics::RigidBody) -> Self {
-        // Self {
-        //     x: value.x,
-        //     y: value.y
-        // }
+        Self {
+            position: Vec2::new(value.position().translation.x, value.position().translation.y),
+            velocity: Vec2::new(value.linvel().x, value.linvel().y),
+            body_type: value.body_type().into()
+        }
     }
 }
 
 
 impl Into<rapier2d::dynamics::RigidBody> for RigidBody {
     fn into(self) -> rapier2d::dynamics::RigidBody {
-        // rapier2d::dynamics::RigidBody {
-        //     x: self.x,
-        //     y: self.y,
-        // }
+
+        match self.body_type {
+            RigidBodyType::Dynamic => {
+                RigidBodyBuilder::dynamic()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::Fixed => {
+                RigidBodyBuilder::fixed()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicPositionBased => {
+                RigidBodyBuilder::kinematic_position_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicVelocityBased => {
+                RigidBodyBuilder::kinematic_velocity_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+        }
     }
 }
 
 impl Into<rapier2d::dynamics::RigidBody> for &RigidBody {
     fn into(self) -> rapier2d::dynamics::RigidBody {
-        // rapier2d::dynamics::RigidBody {
-        //     x: self.x,
-        //     y: self.y,
-        // }
+        match self.body_type {
+            RigidBodyType::Dynamic => {
+                RigidBodyBuilder::dynamic()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::Fixed => {
+                RigidBodyBuilder::fixed()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicPositionBased => {
+                RigidBodyBuilder::kinematic_position_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicVelocityBased => {
+                RigidBodyBuilder::kinematic_velocity_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+        }
     }
 }
 
 impl Into<rapier2d::dynamics::RigidBody> for &mut RigidBody {
     fn into(self) -> rapier2d::dynamics::RigidBody {
-        // rapier2d::dynamics::RigidBody {
-        //     x: self.x,
-        //     y: self.y,
-        // }
+        match self.body_type {
+            RigidBodyType::Dynamic => {
+                RigidBodyBuilder::dynamic()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::Fixed => {
+                RigidBodyBuilder::fixed()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicPositionBased => {
+                RigidBodyBuilder::kinematic_position_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+            RigidBodyType::KinematicVelocityBased => {
+                RigidBodyBuilder::kinematic_velocity_based()
+                    .translation(vector![self.position.x, self.position.y])
+                    .linvel(vector![self.velocity.x, self.velocity.y])
+                    .build()
+            },
+        }
     }
 }
