51b9da2e4fa93108b1afa3809b278a1d1c64fcc7
VoxanyNet <contact@voxany.net>
2024-08-03 14:50:20 -0400

    updater

---
+++ src/updater/main.rs
@@ -0,0 +1,67 @@
+use std::{fs, time::Duration};
+
+fn main() {
+    
+    let program_files_directory = match std::env::var("ProgramFiles") {
+        Ok(program_files_directory) => program_files_directory,
+        Err(error) => {
+            println!("Failed to find program files directory: {}", error.to_string());
+            std::thread::sleep(Duration::from_secs(1));
+            panic!();
+        }
+    };
+
+    let application_directory = format!("{}\\liquidators", program_files_directory);
+
+    // create application directory if it doesnt already exist
+    match fs::create_dir_all(application_directory.clone()) {
+        Ok(_) => {},
+        Err(error) => {
+            println!("Failed to create application directory: {}", error.to_string());
+            std::thread::sleep(Duration::from_secs(1));
+            panic!();
+        },
+    }
+
+    match download("https://liquidators.vxny.io/editor-client.exe") {
+        Ok(bytes) => {
+            
+            match fs::write(application_directory.clone(), bytes) {
+                Ok(_) => {},
+                Err(error) => {
+                    println!("Failed to write download to disk: {}", error.to_string());
+                    std::thread::sleep(Duration::from_secs(1));
+                    panic!();
+                }
+
+            }
+        },
+        Err(error) => {
+            println!("Failed to download latest binary: {}\n\nLaunching existing binary...", error.to_string());
+            std::thread::sleep(Duration::from_secs(1));
+        },
+    }
+
+    match std::process::Command::new(format!("{}\\editor-client.exe", application_directory.clone())).output() {
+        Ok(_) => {},
+        Err(error) => {
+            println!("Failed to run binary: {}", error.to_string());
+            std::thread::sleep(Duration::from_secs(1));
+            panic!();
+        },
+    }
+}
+
+fn download(url: &str) -> Result<Vec<u8>, reqwest::Error> {
+    let response = reqwest::blocking::get(url).expect("Failed to download");
+
+    response.error_for_status_ref()?;
+    
+    let bytes = response.bytes()
+        .expect("Unable to download")
+        .to_vec();
+
+    Ok(bytes)
+
+
+}
